0f50a012f478286821e928a8b4757bcf274ab75a
wangjiaming0909 <604227650@qq.com>
2019-08-21 23:31:07 +0800

    using gin

--- main.go
+++ main.go
@@ -1,59 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"net/url"
 
-	"github.com/wangjiaming0909/GoP/server"
+	"github.com/gin-gonic/gin"
 )
 
-func HttpHandler(w http.ResponseWriter, r *http.Request) {
-	for headerName, headerValue := range r.Header {
-		fmt.Println(headerName)
-		fmt.Println(headerValue)
-	}
+func main() {
+	r := gin.Default()
 
-	w.Write([]byte("Hello World!"))
+	r.GET("/ping", Pong)
+
+	r.Run(":10001")
+	r.Static("/", "/")
 }
 
-func main() {
-	dispather := server.NewDispatcher()
-	err := dispather.RegisterHandler("/", HttpHandler, false)
-	if err != nil {
-		fmt.Println("error: ", err.Error())
-	}
-
-	// resp, err := http.Get("https://www.baidu.com")
-
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// } else {
-	// 	var contents []byte
-	// 	resp.Body.Read(contents)
-	// 	fmt.Println(resp.ContentLength)
-	// 	fmt.Println(contents)
-	// }
-
-	// client := new(http.Client)
-	// response, err := client.Get("https://www.google.com")
-
-	// if err != nil {
-	// 	fmt.Println("error: ", err.Error())
-	// } else {
-	// 	fmt.Println(response.ContentLength)
-	// 	for key, value := range response.Header {
-	// 		fmt.Println(key)
-	// 		fmt.Println(value)
-	// 	}
-	// }
-
-	var uri = new(url.URL)
-	uri.Path = "/"
-	uri.Scheme = "http"
-	uri.Host = "www.baidu.com"
-	fmt.Println(uri.String())
-	http.HandleFunc("/", HttpHandler)
-	http.ListenAndServe(":9090", nil)
-
+func Pong(c *gin.Context) {
+	name := c.Param("name")
+	c.String(http.StatusOK, "Hello %s", name)
 }
--- tests/ServerApi_test.go
+++ tests/ServerApi_test.go
@@ -1,8 +1,12 @@
 package tests
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"testing"
+
+	"github.com/julienschmidt/httprouter"
 
 	"github.com/wangjiaming0909/GoP/server"
 )
@@ -20,4 +24,24 @@ func Test_RegisterHandler(t *testing.T) {
 	if dispatcher.Len() != 1 {
 		t.Error("len should be: 1, actual: ", dispatcher.Len())
 	}
+}
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+func BWithParams(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	for p := range ps {
+		fmt.Fprint(w, p)
+	}
+}
+
+func Test_router(t *testing.T) {
+	r := httprouter.New()
+	r.GET("/", Index)
+	r.GET("/a", Index)
+	r.GET("/a/", Index)
+	r.GET("/b?a=1", BWithParams)
+
+	log.Fatal(http.ListenAndServe(":10001", r))
 }
